732222add6e4dcdb95a7563c4dbd3c3240237c23
Jim Menard <jim@jimmenard.com>
2019-04-03 09:30:41 -0400

    import improvements from the komment project

--- port_midi/src/port_midi.cr
+++ port_midi/src/port_midi.cr
@@ -2,8 +2,65 @@ require "./port_midi/*"
 
 @[Link("portmidi")]
 
+# See also class PortMidi below.
 lib LibPortMidi
-  type PmError = Int32
+  @[Flags]
+  enum Filter
+    Sysex
+    MTC                         # MIDI time code
+    SongPosition
+    SongSelect
+    Unused1
+    Unused2
+    TuneRequest
+    Unused3
+    Clock
+    Tick
+    Play = ((1 << 0x0A) | (1 << 0x0C) | (1 << 0x0B))
+    UndefinedRealtime = (1 << 0x0D)
+    ActiveSensing
+    Reset
+    Note = ((1 << 0x19) | (1 << 0x18))
+    PolyAftertouch = (1 << 0x1A)
+    ControlChange
+    ProgramChange
+    ChannelAftertouch
+    PitchBend
+
+    def realtime
+      ActiveSensing | Sysex | Clock | Play | UndefinedRealtime | Reset | Tick
+    end
+
+    def aftertouch
+      ChannelAftertouch | PolyAftertouch
+    end
+
+    def system_common
+      MTC | SongPosition | SongSelect | TuneRequest
+    end
+  end
+
+  enum PmError : Int32
+    NoError = 0
+    NoData = 0    # A "no error" return that also indicates no data avail.
+    GotData = 1,  # A "no error" return that also indicates data available
+    HostError = -10000
+    InvalidDeviceId,            # out of range or
+                                # output device when input is requested or
+                                # input device when output is requested or
+                                # device is already opened
+    InsufficientMemory,
+    BufferTooSmall,
+    BufferOverflow,
+    BadPtr,                     # PortMidiStream parameter is NULL or
+                                # stream is not opened or
+                                # stream is output when input is required or
+                                # stream is input when output is required
+    BadData,                    # illegal midi data, e.g. missing EOX
+    InternalError,
+    BufferMaxSize               # buffer is already as large as it can be
+  end
+
   type PmTimestamp = UInt32
 
   struct DeviceInfo
@@ -17,14 +74,16 @@ lib LibPortMidi
 
   struct Event
     message : UInt32
-    imtestamp : Int32
+    timestamp : PmTimestamp
   end
+
+  type Stream = Pointer(Void)
 
   fun initialize = Pm_Initialize() : PmError
 
   fun terminate = Pm_Terminate() : PmError
 
-  fun host_error? = Pm_HasHostError(stream : Int64*) : Int32
+  fun host_error? = Pm_HasHostError(stream : Stream) : Int32
 
   fun get_error_text = Pm_GetErrorText(errnum : PmError) : UInt8*
 
@@ -36,32 +95,73 @@ lib LibPortMidi
 
   fun get_device_info = Pm_GetDeviceInfo(device_id : Int32) : DeviceInfo*
 
-  fun open_input = Pm_OpenInput(stream : Int64*, input_device : Int32, input_driver_info : Int32*,
+  fun open_input = Pm_OpenInput(stream : Stream*, input_device : Int32, input_driver_info : Int32*,
                                 buffer_size : Int32, time_proc : Void* -> PmTimestamp,
                                 time_info : Void* -> PmTimestamp) : PmError
 
-  fun open_output = Pm_OpenOutput(stream : Int64*, output_device : Int32, output_driver_info : Int32*,
+  fun open_output = Pm_OpenOutput(stream : Stream*, output_device : Int32, output_driver_info : Int32*,
                                   buffer_size : Int32, time_proc : Void* -> PmTimestamp,
                                   time_info : Void* -> PmTimestamp,
                                   latency : Int32) : PmError
 
-  fun set_filter = Pm_SetFilter(stream : Int64*, filters_bitmask : UInt32) : PmError
+  fun set_filter = Pm_SetFilter(stream : Stream, filters_bitmask : UInt32) : PmError
 
-  fun set_channel_mask = Pm_SetChannelMask(stream : Int64*, bitmask : UInt32) : PmError
+  fun set_channel_mask = Pm_SetChannelMask(stream : Stream, bitmask : UInt32) : PmError
 
-  fun abort_write = Pm_Abort(stream : Int64*) : PmError
+  fun abort_write = Pm_Abort(stream : Stream) : PmError
 
-  fun close_stream = Pm_Close(stream : Int64*) : PmError
+  fun close_stream = Pm_Close(stream : Stream) : PmError
 
-  fun synchronize = Pm_Synchronize(stream : Int64*) : PmError
+  fun synchronize = Pm_Synchronize(stream : Stream) : PmError
 
-  fun midi_read = Pm_Read(stream : Int64*, buffer : Pointer(Pointer(Event)), length : Int32) : PmError
+  fun midi_read = Pm_Read(stream : Stream, buffer : Pointer(Event), length : Int32) : Int32
 
-  fun poll = Pm_Poll(stream : Int64*) : PmError
+  fun poll = Pm_Poll(stream : Stream) : PmError
 
-  fun midi_write = Pm_Write(stream : Int64*, buffer : Event*, length : Int32) : PmError
+  fun midi_write = Pm_Write(stream : Stream, buffer : Event*, length : Int32) : PmError
 
-  fun midi_write_short = Pm_WriteShort(stream : Int64, when_tstamp : Int32, msg : UInt32) : PmError
+  fun midi_write_short = Pm_WriteShort(stream : Stream, when_tstamp : Int32, msg : UInt32) : PmError
 
-  fun midi_write_sysex = Pm_WriteSysEx(stream : Int64, when_tstamp : Int32, msg : UInt8*) : PmError
+  fun midi_write_sysex = Pm_WriteSysEx(stream : Stream, when_tstamp : Int32, msg : UInt8*) : PmError
+end
+
+class PortMidi
+  def self.status?(b : UInt32)
+    b & 0x80
+  end
+
+  def self.realtime?(b : UInt32)
+    b >= 0xf8
+  end
+
+  def self.message(status, data1, data2)
+    ((((data2) << 16) & 0xFF0000) |
+     (((data1) << 8) & 0xFF00) |
+     ((status) & 0xFF))
+  end
+
+  def self.status(message : UInt32)
+    message & 0xff
+  end
+
+  def self.data1(message : UInt32)
+    (message >> 8) & 0xff
+  end
+
+  def self.data2(message : UInt32)
+    (message >> 16) & 0xff
+  end
+
+  def self.bytes(event : LibPortMidi::Event) : StaticArray(UInt8, 4)
+    bytes(event.message)
+  end
+
+  def self.bytes(message : UInt32) : StaticArray(UInt8, 4)
+    buf = uninitialized UInt8[4]
+    buf[0] = (message & 0xff).to_u8
+    buf[1] = ((message >> 8) & 0xff).to_u8
+    buf[2] = ((message >> 16) & 0xff).to_u8
+    buf[3] = ((message >> 24) & 0xff).to_u8
+    buf
+  end
 end
